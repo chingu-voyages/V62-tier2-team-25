7716f175b537e9e7571d551935c092188fdfef1e
Jose Cruz <159391963+Cruz3196@users.noreply.github.com>
2026-09-07 21:46:22 -0700

    footer component added with link leading to team 25 repo

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import LandingPage from "./components/LandingPage";
+import { Footer } from "./components/layout/Footer";
 import "./App.css";
 
 function App() {
   return (
     <>
       <LandingPage />
+      <Footer />
     </>
   );
 }
---
+++ vite-project/src/components/layout/Footer.jsx
@@ -0,0 +1,14 @@
+export const Footer = () => {
+  return (
+    <div>
+      <footer className="text-center p-4">
+        <a
+          href="https://github.com/chingu-voyages/V62-tier2-team-25"
+          className="text-md hover:text-blue-500 mb-4"
+        >
+          View Team Github Repository
+        </a>
+      </footer>
+    </div>
+  );
+};
