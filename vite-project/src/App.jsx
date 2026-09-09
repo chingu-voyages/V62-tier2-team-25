db13ceed5f4d4afa3b7190e2f737c3cf8da0dfb2
Jose Cruz <159391963+Cruz3196@users.noreply.github.com>
2026-09-09 14:16:07 -0700

    Revert "feat: Add DevTrajectory header component to layout"

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import LandingPage from "./components/LandingPage";
-import Header from './components/Header';
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Header />
       <LandingPage />
     </>
   );
