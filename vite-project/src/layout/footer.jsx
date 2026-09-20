c0ec8bb3bb13e6e19f8a98d9cf5c32852df52555
Damaris <damaris@MacBook-Pro-de-Bertha.local>
2026-09-20 12:24:34 -0700

    feat: change background color to white and text to black

--- vite-project/src/layout/footer.jsx
+++ vite-project/src/layout/footer.jsx
@@ -1,10 +1,10 @@
 export const Footer = () => {
   return (
-    <footer className="w-full bg-slate-900 border-b border-slate-800 py-6 px-8 shadow-sm">
+    <footer className="w-full bg-slate-50 border-b border-slate-800 py-6 px-8 shadow-sm">
       <div className="max-w-7xl mx-auto flex flex-col sm:flex-row justify-center text-center gap-4">
         <a
           href="https://github.com/chingu-voyages/V62-tier2-team-25"
-          className="text-2xl sm:text-3xl font-extrabold text-white tracking-tight"
+          className="text-2xl sm:text-3xl font-extrabold text-black tracking-tight"
         >
           View Team Github Repository
         </a>
